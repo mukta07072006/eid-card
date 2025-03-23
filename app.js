357c559c7a28731a54cdd529c6439caf8153f07e
Moshud Muktadir <worldamity994@gmail.com>
2025-03-24 03:43:47 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+   const path = require('path');
+
+   const app = express();
+   const PORT = 3000;
+
+   // Set up EJS as the view engine
+   app.set('view engine', 'ejs');
+   app.set('views', path.join(__dirname, 'views'));
+
+   // Serve static files (images, CSS, etc.)
+   app.use(express.static(path.join(__dirname, 'public')));
+
+   // Homepage
+   app.get('/', (req, res) => {
+       res.render('index');
+   });
+
+   // Dynamic routes for each person
+   app.get('/:name', (req, res) => {
+       const name = req.params.name;
+       const imagePath = `/images/${name}.jpg`; // Assumes image names match the URL
+       res.render('card', { name, imagePath });
+   });
+
+   // Start the server
+   app.listen(PORT, () => {
+       console.log(`Server is running on http://localhost:${PORT}`);
+   });
